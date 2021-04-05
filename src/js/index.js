54cb1532fc564c74d8f91dba877d8acb3acd6e43
StarBurst911 <Bin.ni23@sitechhs.com>
2021-04-04 20:55:31 -0400

    erter4wtg4ew

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log("connected");
+const startButton = document.getElementById("starting-btn");
+const questionContainerElement = document.getElementById("questions");
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+}
+
+function NextQuestion() {}
+
+function SelectAnswer() {}
